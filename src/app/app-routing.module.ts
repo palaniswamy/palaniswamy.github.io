06d541829b495d4edd202764318256b59ecd6a42
Pal Ramasamy <palaniswamy@users.noreply.github.com>
2021-10-07 22:46:06 -0400

    App Routing to a component as well as a lazily loaded module works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./home/home.component";
-import { SkillsComponent } from "./skills/skills.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'skills', component: SkillsComponent},
+  {path: '', component: HomeComponent},
+  {path: 'skills', loadChildren: () => import('./skills/skills.module').then(m => m.SkillsModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SkillsComponent } from './skills/skills.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsComponent,
     HomeComponent
   ],
   imports: [
